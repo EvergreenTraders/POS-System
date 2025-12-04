257f50a8a0f0920892a0969e631ba89bb248564c
priyaguntupalli5 <guntupallisaipriya@gmail.com>
2025-12-04 18:53:40 -0400

    fixed ebs health issues

--- backend/migrate.js
+++ backend/migrate.js
@@ -9,6 +9,9 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT || 5432,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 // Migration files in order
